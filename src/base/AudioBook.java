c108a42ffb1ed93a631fe87fe5d6693283c519df
na <na9173@suwon.ac.kr>
2023-10-31 17:19:24 +0900

    도서 요구사항 수정 -1

---
+++ src/base/AudioBook.java
@@ -0,0 +1,31 @@
+package base;
+
+public class AudioBook extends Book{
+    private String fileSize;
+    private String language;
+    private int playTime;
+
+    public String getFileSize() {
+        return fileSize;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public int getPlayTime() {
+        return playTime;
+    }
+
+    public void setFileSize(String fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public void setPlayTime(int playTime) {
+        this.playTime = playTime;
+    }
+}
---
+++ src/base/EBook.java
@@ -0,0 +1,15 @@
+package base;
+
+public class EBook extends Book {
+    private String fileSize;
+
+    public String getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(String fileSize) {
+        this.fileSize = fileSize;
+    }
+
+}
+
